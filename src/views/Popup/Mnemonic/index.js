9cffc748b7c5ebd550a87c2a1744845aa2fd6577
Hoang Nam <dinhhoangnam998@gmail.com>
2020-11-13 16:07:08 +0700

    add overlay on mnemonic ok

--- src/views/Popup/Mnemonic/index.js
+++ src/views/Popup/Mnemonic/index.js
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import Container from "../shared/Container";
 import Header from "../shared/Header";
+import LockOpenIcon from "@material-ui/icons/LockOpen";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,15 +14,41 @@ const useStyles = makeStyles((theme) => ({
   },
   mnemonic: {
     marginTop: theme.spacing(2),
+    filter: (props) => (props.blur ? "blur(5px)" : "blur(0)"),
+    "& .MuiFormLabel-root.Mui-disabled": {
+      color: "black",
+    },
   },
   button: {
     marginBottom: "0",
   },
+  bounder: {
+    position: "relative",
+  },
+  overlay: {
+    position: "absolute",
+    zIndex: "1",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "rgba(102, 102, 102, 0.7)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "white",
+    fontSize: "1.2rem",
+  },
+  overideDisable: {
+    color: "black ",
+  },
 }));
 
 export default function Mnemonic() {
-  const cls = useStyles();
   const [redirect, setRedirect] = useState(null);
+  const [overlay, setOverlay] = useState(true);
+  const cls = useStyles({ blur: overlay });
   const mnemonic = useSelector((state) => state.mnemonic);
   return (
     <div>
@@ -34,16 +61,32 @@ export default function Mnemonic() {
           <Typography>
             <b>CẢNH BÁO:</b> Không bao giờ tiết lộ mã này. Bất kì ai có mã này sẽ có quyền sử dụng các tài khoản của bạn.
           </Typography>
-          <TextField
-            className={cls.mnemonic}
-            variant="outlined"
-            multiline
-            rows={5}
-            fullWidth
-            label="Mã mnemonic"
-            defaultValue={mnemonic}
-            InputLabelProps={{ shrink: true }}
-          ></TextField>
+          <Box className={cls.bounder}>
+            {overlay ? (
+              <div
+                className={cls.overlay}
+                onClick={(e) => {
+                  setOverlay(false);
+                }}
+              >
+                <LockOpenIcon fontSize="large"></LockOpenIcon>
+                Click để xem mã Mnemonic
+              </div>
+            ) : null}
+            <TextField
+              className={cls.mnemonic}
+              InputProps={{ classes: { disabled: cls.overideDisable } }}
+              InputLabelProps={{ shrink: true }}
+              variant="outlined"
+              multiline
+              rows={5}
+              fullWidth
+              label="Mã mnemonic"
+              defaultValue={mnemonic}
+              disabled
+            ></TextField>
+          </Box>
+
           <Button
             className={cls.button}
             variant="contained"
