39b2c1cc26b8df15ec06d09de641c262b9549760
DinhHoangNam <dinhhoangnam162793@gmail.com>
2021-08-05 18:06:30 +0700

    :pencil2: add more account to devmodeMap

--- src/views/Popup/redux.js
+++ src/views/Popup/redux.js
@@ -9,16 +9,17 @@ const devmodeMap = {
   BGD: "6cebf871e936d15b6540dc714dcff176839f73359d30ae49ae8ec1d44bd276db",
   BKHN: "6fd04410d4fe5cf4d67e2c3c5f1565aab576199bfc8c4011dc5b3054375d8953",
   KTQD: "6342c8682a84c9503c56294631fda268db12e084998cca5d0b5367bf47b14e3d",
-  DHXD: "b77c57b097787b91faac4eb875fd6ab3a0e11257d7b722edfb010cd3cba42b2c",
+  // DHXD: "b77c57b097787b91faac4eb875fd6ab3a0e11257d7b722edfb010cd3cba42b2c",
+  "Trường cấp 1 Hương Mạc": "0b4d2f6255f771e511f75c2cd8b4f01df5d0caa7966b53ab0e67c547e40a114e",
+  "Trường cấp 2 Hương Mạc": "7df87fc9f422e242867973fb62b4492cddece9368dcfde7eafc59266667a74df",
   "GV Đỗ Bá Lâm": "1c84984dcaef77eb4e6b5e0d68662cc569118334a807e722e94b1e34cd21ca72",
   "GV Nguyễn Bình Minh": "7b1ec199c4827b0f46674fec95ba1a337f0042b285755ad5acdeeb64c0f8cf7d",
   "GV Đào Thành Chung": "1c2c728f6673a42630833f22790903275e79cf0702280018428d0f9e0a7584df",
   "SV Đỗ Hòa An": "dcbc581f8a1f73458eea31311ede5b16721628496f53ecf3c0e4cef4cbe02a4e",
   "SV Bùi Hải Anh": "1887157aeee14c71ab0b773912a83fbb9bba7c45c3c034135898e764b6599497",
-  "SV Bùi Quang Anh": "8f30daa6af9de2b2fc5d8169dd5b55bed30501239a1117a11c638969bbaa7fcb",
-  "Tiểu Học Hương Mạc 1": "0b4d2f6255f771e511f75c2cd8b4f01df5d0caa7966b53ab0e67c547e40a114e",
-  "Trung học cơ sở HM1": "7df87fc9f422e242867973fb62b4492cddece9368dcfde7eafc59266667a74df",
-  "GV Nguyễn Thị An - HM1": "38dee1c5dc44d7c65de2255a7c06734c23fcc80a7bf27f963003461480bd8b8d",
+  "GV Nguyễn Thị An - Cấp 1 HM": "38dee1c5dc44d7c65de2255a7c06734c23fcc80a7bf27f963003461480bd8b8d",
+  "GV Trần Văn Bình - Cấp 2 HM": "535c36122d78a32fd07dc6851952edcb08173441be77d3bd31165b8548f45a21",
+  // "SV Bùi Quang Anh": "8f30daa6af9de2b2fc5d8169dd5b55bed30501239a1117a11c638969bbaa7fcb",
 };
 
 const devmodeAccounts = Object.entries(devmodeMap).map((entry) => ({
